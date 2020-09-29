ff10841214f0bdf2bdc028ab1ecf52d39d5b3667
dappklex <dappklex@outlook.com>
2020-09-29 20:04:47 +0800

    0929:finished kdtree search

--- 第三章：K近邻/knn.py
+++ 第三章：K近邻/knn.py
@@ -35,25 +35,93 @@ train = loadData('../mnist/mnist_train.csv')
 # %%
 # build kd tree
 class kd(object):
-    def __init__(self, data, j, stds, parent):
+    def __init__(self, data, j, stds, parent, type):
         self.parent = parent
         self.l = stds.index[j%len(stds)]
+        self.stds = stds
+        self.type = type
         k = int(np.floor(len(data)/2))
         sorted_data = data.sort_values(self.l)
         self.node = sorted_data.iloc[k, :]
         leftdata = sorted_data.iloc[:k, :]
         rightdata = sorted_data.iloc[k+1:, :]
-        print(j, len(leftdata), len(rightdata))
         if len(leftdata)>0:
-            self.left = kd(leftdata, j+1, stds, self)
+            self.left = kd(leftdata, j+1, stds, self, 'left')
         else:
             self.left = None 
         if len(rightdata)>0:
-            self.right = kd(rightdata, j+1, stds, self)
+            self.right = kd(rightdata, j+1, stds, self, 'right')
         else:
             self.right = None 
+            
+    def findChild(self, data):
+        # print(self.type)
+        if self.left!=None and self.right!=None:
+            if data[self.l]<=self.node[self.l]:
+                t = self.left.findChild(data)
+            else:
+                t = self.right.findChild(data)
+        elif self.left!=None:
+            t = self.left.findChild(data)
+        elif self.right!=None:
+            t = self.right.findChild(data)
+        else:
+            # global klist
+            # klist.append((self.node, sum((self.node[:-1]-data[:-1])**2)))
+            # klist = sorted(klist, key=lambda x:x[1])
+            t = self 
+        return t 
+
 #%%
 stds = np.std(train.iloc[:,:-1], axis=0).sort_values(ascending=False)
-kdtree = kd(train, 0, stds, None)
+kdtree = kd(train, 0, stds, None, 'root')
+#%%
+k = 3
+klist = []
+data = test.iloc[0, :]
+def dist(node, data):
+    return sum((node[:-1]-data[:-1])**2)
 
+#%%
+# find first node
+node = kdtree.findChild(data)
+klist.append((node, dist(node.node, data)))
+klist = sorted(klist, key=lambda x:x[1])[:k]
+cur_max = max([x[1] for x in klist])
+checked_node = []
+
+#%%
+while True:
+    print(node)
+    if node.type=='root':
+        print('finished')
+        break 
+    else:
+        parent = node.parent
+        if parent in checked_node:
+            node = parent
+            continue
+        else:
+            checked_node.append(parent)
+        
+        if dist(parent.node, data)<cur_max:
+            klist.append((parent, dist(parent.node, data)))
+            klist = sorted(klist, key=lambda x:x[1])[:k]
+            cur_max = max([x[1] for x in klist])
+        
+        if abs(parent.node[parent.l]-data[parent.l])<cur_max:
+            if node.type=='right':
+                next_node = parent.left
+            else:
+                next_node = parent.right 
+            
+            if next_node==None:
+                node = parent 
+            else:
+                tmp = next_node.findChild(data)
+                if tmp==None:
+                    node = next_node
+                else:
+                    node = tmp 
+            
 # %%
